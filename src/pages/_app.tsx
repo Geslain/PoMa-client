5300f640684e8e46a4cfdf5470c5b76d86d7b2b7
Geslain Dahan <geslain.dahan@spaceable.org>
2024-03-28 03:01:00 +0700

    feat(projects): improve existing page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import Layout from "@/pages/layout";
 import { ReactNode } from "react";
+import "../app/globals.css";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps & { Component: AppProps["Component"] & { getLayout?: (page: ReactNode) => ReactNode } }) {
+}: AppProps & {
+  Component: AppProps["Component"] & {
+    getLayout?: (page: ReactNode) => ReactNode;
+  };
+}) {
   const getLayout =
     Component.getLayout ?? ((page: ReactNode) => <Layout>{page}</Layout>);
   return (
