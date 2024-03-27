5ef77bc91064bc05f089410709f7cfaedc9cecf5
Geslain Dahan <geslain.dahan@spaceable.org>
2024-03-28 04:20:03 +0700

    feat(projects): fix hooks for avoiding memory leak loop

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -7,30 +7,36 @@ const useProjects = () => {
   const session = useSession();
   const accessToken = session.data?.user.accessToken;
 
-  const fetchProject = useCallback(
-    async function (projectUrl: string, init: Parameters<typeof fetch>[1]) {
-      if (!accessToken) return null;
-      return await (
-        await fetchData(`${url}${projectUrl}`, {
-          headers: {
-            authorization: `Bearer ${accessToken}`,
-          },
-          ...init,
-        })
-      ).json();
-    },
+  const fetchProject = async function (
+    projectUrl: string,
+    init: Parameters<typeof fetch>[1],
+  ) {
+    if (!accessToken) return null;
+    return await (
+      await fetchData(`${url}${projectUrl}`, {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+        ...init,
+      })
+    ).json();
+  };
+
+  const getProjects = useCallback(
+    () =>
+      fetchProject("", {
+        method: "GET",
+      }),
     [accessToken],
   );
 
-  const getProjects = () =>
-    fetchProject("", {
-      method: "GET",
-    });
-
-  const getProject = (id: string) =>
-    fetchProject(id, {
-      method: "GET",
-    });
+  const getProject = useCallback(
+    (id: string) =>
+      fetchProject(`/${id}`, {
+        method: "GET",
+      }),
+    [accessToken],
+  );
 
   return { getProjects, getProject };
 };
